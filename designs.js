c19df1e8deb07997b72e4a569455f9b0e76dafe8
Joshua-Chu <jpchu@up.edu.ph>
2022-01-01 19:34:28 +0800

    feat: add helper functions

---
+++ designs.js
@@ -0,0 +1,46 @@
+// Application State
+
+const STATE = {
+	gridHeight: 1,
+	gridWidth: 1,
+	gridColor: "#000000",
+};
+
+//DOM Node References
+
+const $inputHeight = document.getElementById("inputHeight");
+const $inputWidth = document.getElementById("inputWidth");
+const $sizePicker = document.getElementById("sizePicker");
+const $colorPicker = document.getElementById("colorPicker");
+const $pixelCanvas = document.getElementById("pixelCanvas");
+
+/**
+ * @description extract the inputs given by the user
+ * @returns {string, string, string} grid row, cells per row, cell color when clicked
+ */
+
+const getInputs = () => {
+	const { gridHeight, gridWidth, gridColor } = {
+		gridHeight: Number($inputHeight.value),
+		gridWidth: Number($inputWidth.value),
+		gridColor: $colorPicker.value,
+	};
+
+	return { gridHeight, gridWidth, gridColor };
+};
+
+/**
+ * @description generates an array of numbers from 1 to n
+ * @param {number} num
+ * @returns {number[]} array of numbers
+ */
+
+const generateArray = (num) => Array.from({ length: num }, (v, k) => k + 1);
+
+/**
+ * @description resets html content of pixel Canvas
+ */
+
+const canvasReset = () => {
+	$pixelCanvas.innerHTML = "";
+};
